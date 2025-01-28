ed34b9ce665bd1414f85bc1f9c8221992bdf932f
Rinav01 <23051451@kit.ac.in>
2025-01-28 09:24:46 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -25,8 +28,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  
-
   final String title;
 
   @override
@@ -34,41 +35,91 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  File? image;
+  late ImagePicker imagePicker;
+  late ImageLabeler labeler;
+  @override
+  void initState() {
+    super.initState();
+    imagePicker = ImagePicker();
+    final ImageLabelerOptions options =
+        ImageLabelerOptions(confidenceThreshold: 0.6);
+    labeler = ImageLabeler(options: options);
+  }
 
-  void _incrementCounter() {
+  chooseImage() async {
+    XFile? selectedImage =
+        await imagePicker.pickImage(source: ImageSource.gallery);
+    if (selectedImage != null) {
+      performImageLabelling();
+      setState(() {
+        image = File(selectedImage.path);
+      });
+    }
+  }
+
+  captureImage() async {
+    XFile? selectedImage =
+        await imagePicker.pickImage(source: ImageSource.camera);
+    if (selectedImage != null) {
+      performImageLabelling();
+      setState(() {
+        image = File(selectedImage.path);
+      });
+    }
+  }
+
+  String results = "";
+  performImageLabelling() async {
+    results = "";
+    InputImage inputImage = InputImage.fromFile(image!);
+
+    final List<ImageLabel> labels = await labeler.processImage(inputImage);
+
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+      print(text + "   " + confidence.toString());
+      results += text + "  " + confidence.toStringAsFixed(2) + "\n";
+    }
+
     setState(() {
-      
-      _counter++;
+      results;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              image == null
+                  ? const Icon(
+                      Icons.image_outlined,
+                      size: 100,
+                    )
+                  : Image.file(image!),
+              ElevatedButton(
+                onPressed: () {
+                  chooseImage();
+                },
+                onLongPress: () {
+                  captureImage();
+                },
+                child: const Text('Choose/Capture'),
+              ),
+              Text(results),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
